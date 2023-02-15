67719b206fe701b12a0f940197932b586c263c41
Jhed Mendoza <jhed.mendoza@hotmail.com>
2023-02-15 16:45:40 +0800

    Added email integration

--- assets/js/email.js
+++ assets/js/email.js
@@ -5,16 +5,29 @@
     function init() {
 
         const API_URL = 'https://api.emailjs.com/api/v1.0/email/send';
+        const SERVICE_ID = 'service_vcc3fdf';
+        const TEMPLATE_ID = 'jm_xftqc27';
+        const USER_ID = 'rke7kU1n5NYJuQ_vk';
 
         $('.send-btn').on('click', function (e) {
             e.preventDefault();
 
             if (xhr && xhr.readyState != 4) xhr.abort();
 
+            var data = {
+                service_id: SERVICE_ID,
+                template_id: TEMPLATE_ID,
+                user_id: USER_ID,
+                template_params: {
+                    'from_name': 'James',
+                    'message': 'Test'
+                }
+            };
+
             xhr = $.ajax({
-                url: '',
+                url: API_URL,
                 type: 'POST',
-                data: {},
+                data: JSON.stringify(data),
                 beforeSend: function () {
                     $('.send-btn').prop('disabled', true);
                     $('.send-btn .spinner-border').show();
