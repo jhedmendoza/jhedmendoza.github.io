c520598cb5f79b9223303510664bc4b59e8f00ea
Jhed Mendoza <jhed.mendoza@hotmail.com>
2023-02-17 15:43:39 +0800

    Added email service

--- assets/js/email.js
+++ assets/js/email.js
@@ -4,10 +4,10 @@
 
     function init() {
 
-        const API_URL = 'https://api.emailjs.com/api/v1.0/email/send';
-        const SERVICE_ID = 'service_vcc3fdf';
+        const API_URL     = 'https://api.emailjs.com/api/v1.0/email/send';
+        const SERVICE_ID  = 'service_vcc3fdf';
         const TEMPLATE_ID = 'jm_xftqc27';
-        const USER_ID = 'rke7kU1n5NYJuQ_vk';
+        const USER_ID     = 'rke7kU1n5NYJuQ_vk';
 
         $('.send-btn').on('click', function (e) {
             e.preventDefault();
@@ -19,21 +19,25 @@
                 template_id: TEMPLATE_ID,
                 user_id: USER_ID,
                 template_params: {
-                    'from_name': 'James',
-                    'message': 'Test'
+                    'from_name' : $('#name').val().trim(),
+                    'from_email': $('#email').val().trim(),
+                    'message'   : $('#message').val().trim()
                 }
             };
 
             xhr = $.ajax({
                 url: API_URL,
                 type: 'POST',
+                contentType: 'application/json',
                 data: JSON.stringify(data),
+                
                 beforeSend: function () {
                     $('.send-btn').prop('disabled', true);
                     $('.send-btn .spinner-border').show();
                     $('.send-btn .btn-msg').text('Sending...');
                 },
                 success: function (response) {
+                    alert('Your message has been sent!');
                 },
                 complete: function () {
                     $('.send-btn').removeAttr('disabled');
@@ -41,7 +45,8 @@
                     $('.send-btn .btn-msg').text('Send');
                 },
                 error: function (xhr) {
-                    alert("Something went wrong. Please try again later.");
+                    console.log(xhr);
+                   // alert("Something went wrong. Please try again later.");
                 }
             });
         })
